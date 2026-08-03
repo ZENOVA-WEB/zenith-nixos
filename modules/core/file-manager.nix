da799603753cca1abecdd20648af03216823d73b
zaeem <zaeemali272@gmail.com>
2026-08-04 03:27:30 +0500

    Trying to fix zenith

--- modules/core/file-manager.nix
+++ modules/core/file-manager.nix
@@ -12,4 +12,12 @@
   programs.xfconf.enable = true;
   services.tumbler.enable = true; 
   services.gvfs.enable = true;    
+
+  # Declaratively configure Thunar settings via Xfconf XML backend
+  environment.etc."xdg/xfce4/xfconf/xfce-perchannel-xml/thunar.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <channel name="thunar" version="1.0">
+      <property name="last-menubar-visible" type="bool" value="false"/>
+    </channel>
+  '';
 }
