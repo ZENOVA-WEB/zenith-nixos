f776dbcafe8cf94841522defb53d53fee043a1bd
zaeem <zaeemali272@gmail.com>
2026-08-11 23:33:55 +0500

    fix: disable Home Manager Hyprland config generation to prevent file collisions

--- modules/home/quickshell.nix
+++ modules/home/quickshell.nix
@@ -6,7 +6,11 @@
     package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
   };
 
+  # Disable Home Manager hyprland.conf generator to prevent collision with out-of-store hypr directory link
+  wayland.windowManager.hyprland.enable = lib.mkForce false;
+
   # Real-time out-of-store live development symlinks
+
   xdg.configFile."quickshell".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/zenith/zenith-shell");
   xdg.configFile."hypr".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "/home/${vars.user}/zenith/Hyprland-dots");
 }
